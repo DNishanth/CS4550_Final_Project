7f48a6582bac4008a04839c57f735df869eb6a08
Nishanth Duraiarasu <dnishanth99@gmail.com>
2020-05-29 17:12:24 -0400

    Added details page

--- src/components/ResultInfoComponent.js
+++ src/components/ResultInfoComponent.js
@@ -2,17 +2,39 @@ import React from "react";
 
 export default class ResultInfoComponent extends React.Component {
 
+    componentDidMount() {
+        const imdb_id = this.props.match.params.imdbID
+        const tmdb_key = "54fa45f317f011fa4fd57c4d1840485c"
+        const tmdb_img_path = "https://image.tmdb.org/t/p/w200"
+        fetch(`https://api.themoviedb.org/3/find/${imdb_id}?api_key=${tmdb_key}&language=en-US&external_source=imdb_id`)
+            .then(response => response.json())
+            .then(result => {
+                this.setState({
+                    result: result.movie_results[0],
+                    poster_path: tmdb_img_path + result.movie_results[0].poster_path
+                })
+            })
+    }
+
     state = {
-        result: {title: "a", year: "a", score: "a", }
-        // this.props.result
+        result: {},
+        poster_path: ""
     }
 
     render() {
         return(
             <div>
-                <h3>{this.state.result.title}</h3>
-                <h3>{this.state.result.year}</h3>
-                <h3>{this.state.result.score}</h3>
+                <h1> 
+                    {this.state.result.title}
+                    {" (" + this.state.result.release_date + ")"}
+                </h1>
+                <img src={this.state.poster_path}/>
+                <h4>
+                    {this.state.result.vote_average}
+                </h4>
+                <p> 
+                    {this.state.result.overview}
+                </p>
             </div>
         )
     }
