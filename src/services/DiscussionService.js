d15c454b4958d3f3e5f6b15df7ee7313b0acfdc0
Nishanth Duraiarasu <dnishanth99@gmail.com>
2020-06-19 17:57:20 -0400

    Updated DiscussionServicePath to match heroku

--- src/services/DiscussionService.js
+++ src/services/DiscussionService.js
@@ -1,9 +1,9 @@
 export const findPostsForDiscussion = (showId) =>
-  fetch(`http://localhost:8080/api/discussions/${showId}`)
+  fetch(`https://wbdv-team18-final-project.herokuapp.com/api/discussions/${showId}`)
     .then(response => response.json())
 
 export const createPost = (showId, post) =>
-  fetch(`http://localhost:8080/api/discussions/${showId}/posts`, {
+  fetch(`https://wbdv-team18-final-project.herokuapp.com/api/discussions/${showId}/posts`, {
     method: 'POST',
     body: JSON.stringify(post),
     headers: {
@@ -13,7 +13,7 @@ export const createPost = (showId, post) =>
     .then(response => response.json())
 
 export const createShowDiscussion = (show) =>
-  fetch(`http://localhost:8080/api/discussions`, {
+  fetch(`https://wbdv-team18-final-project.herokuapp.com/api/discussions`, {
     method: 'POST',
     body: JSON.stringify(show),
     headers: {
@@ -23,5 +23,5 @@ export const createShowDiscussion = (show) =>
     .then(response => response.json())
 
 export const findShowByImdb = (imdb) =>
-  fetch(`http://localhost:8080/api/shows/${imdb}`)
+  fetch(`https://wbdv-team18-final-project.herokuapp.com/api/shows/${imdb}`)
     .then(response => response.json())
