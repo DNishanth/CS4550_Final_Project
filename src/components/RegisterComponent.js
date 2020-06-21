3812aeb756498def6e917162a7e9965111e23073
Nishanth Duraiarasu <dnishanth99@gmail.com>
2020-06-21 01:59:12 -0400

    Fixed selected warning

--- src/components/RegisterComponent.js
+++ src/components/RegisterComponent.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default class Register extends React.Component {
     state = {
@@ -12,7 +12,7 @@ export default class Register extends React.Component {
     }
     register = () => {
         fetch("http://localhost:8080/api/register", {
-        // fetch("https://wbdv-team18-final-project.herokuapp.com/api/register", {
+            // fetch("https://wbdv-team18-final-project.herokuapp.com/api/register", {
             body: JSON.stringify({
                 username: this.state.username, password: this.state.password,
                 role: this.state.role, firstName: this.state.firstName,
@@ -32,38 +32,53 @@ export default class Register extends React.Component {
             <div>
                 {/* add css padding */}
                 <h1>Register</h1>
-                <input
-                    placeholder="Username"
-                    onChange={(e) => this.setState({ username: e.target.value })}
-                    className="form-control" />
-                <input
-                    placeholder="Password"
-                    onChange={(e) => this.setState({ password: e.target.value })}
-                    className="form-control" />
-                <select
-                    onChange={(e) => this.setState({ role: e.target.value })}
-                    value={this.state.role}
-                    className="custom-select custom-select">
-                    <option selected disabled> Choose User Role</option>
-                    <option value="LEADER">
-                        Group Leader
-                    </option>
-                    <option value="MEMBER">
-                        Member
-                    </option>
-                </select>
-                <input
-                    placeholder="First Name"
-                    onChange={(e) => this.setState({ firstName: e.target.value })}
-                    className="form-control" />
-                <input
-                    placeholder="Last Name"
-                    onChange={(e) => this.setState({ lastName: e.target.value })}
-                    className="form-control" />
-                <input
-                    placeholder="Email"
-                    onChange={(e) => this.setState({ email: e.target.value })}
-                    className="form-control" />
+
+                <div className="form-group row">
+                    <input
+                        placeholder="Username"
+                        onChange={(e) => this.setState({ username: e.target.value })}
+                        className="form-control" />
+                </div>
+
+                <div className="form-group row">
+                    <input
+                        placeholder="Password"
+                        onChange={(e) => this.setState({ password: e.target.value })}
+                        className="form-control" />
+                </div>
+
+                <div className="form-group row">
+                    <select
+                        onChange={(e) => this.setState({ role: e.target.value })}
+                        defaultValue={`CHOOSE`}
+                        className="custom-select custom-select">
+                        <option disabled value="CHOOSE"> Choose User Role</option>
+                        <option value="LEADER"> Group Leader </option>
+                        <option value="MEMBER"> Member </option>
+                    </select>
+                </div>
+
+                <div className="form-group row">
+                    <input
+                        placeholder="First Name"
+                        onChange={(e) => this.setState({ firstName: e.target.value })}
+                        className="form-control" />
+                </div>
+
+                <div className="form-group row">
+                    <input
+                        placeholder="Last Name"
+                        onChange={(e) => this.setState({ lastName: e.target.value })}
+                        className="form-control" />
+                </div>
+
+                <div className="form-group row">
+                    <input
+                        placeholder="Email"
+                        onChange={(e) => this.setState({ email: e.target.value })}
+                        className="form-control" />
+                </div>
+
                 <button
                     onClick={this.register}
                     className="btn btn-primary">
