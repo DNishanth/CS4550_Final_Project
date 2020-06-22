5e2eeef04a854043fc7176ec66afa6f4b022d0bf
jchapin <chapin.j@husky.neu.edu>
2020-06-22 00:45:43 -0400

    validation well designed

--- src/components/LoginComponent.js
+++ src/components/LoginComponent.js
@@ -2,10 +2,19 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default class LoginComponent extends React.Component {
-    state = {
-        username: '',
-        password: ''
+    constructor() {
+        super();
+        this.state = {
+            username: "",
+            password: "",
+
+            validUsername: true,
+            validPassword: true,
+            invalidCredentials: false
+        }
     }
+
+
     login = () => {
         // fetch("https://wbdv-team18-final-project.herokuapp.com/api/login", {
         fetch("http://localhost:8080/api/login", {
@@ -20,49 +29,99 @@ export default class LoginComponent extends React.Component {
             credentials: "include"
         }).then(response => response.json())
             .catch(e => {
+                this.setState({
+                    invalidCredentials: true
+                })
                 this.props.history.push("/login")
             })
             .then(currentUser => {
                 if (currentUser)
                     this.props.history.push("/profile/watchlist")
             })
+    }
 
+    validate = () => {
+        this.setState({
+            validUsername: this.state.username !== "",
+            validPassword: this.state.password !== "",
+        }, () => {
+            if (this.state.validUsername && this.state.validPassword)
+                this.login()
+            })
     }
 
     render() {
         return (
             <div>
                 <h1>Login</h1>
-
                 <form>
-                    <div className="form-group">
-                        <label htmlFor="usernameInput">Username</label>
-                        <input
-                            required
-                            id="usernameInput"
-                            className="form-control"
-                            placeholder="Enter Username"
-                            onChange={(e) => this.setState({ username: e.target.value })} />
-                    </div>
 
-                    <div className="form-group">
-                        <label htmlFor="passwordInput">Password</label>
-                        <input
-                            type="password"
-                            required
-                            id="passwordInput"
-                            className="form-control"
-                            placeholder="Enter Password"
-                            onChange={(e) => this.setState({ password: e.target.value })} />
-                    </div>
+                    {
+                        this.state.invalidCredentials &&
+                        <p className="text-danger mt-3 mb-1">Invalid Credentials</p>
+                    }
+                    {
+                        this.state.validUsername &&
+                        <div className="form-group">
+                            <label htmlFor="usernameInput">Username</label>
+                            <input
+                                id="usernameInput"
+                                className="form-control"
+                                placeholder="Enter Username"
+                                onChange={(e) => this.setState({ username: e.target.value })} />
+                        </div>
+                    }
+                    {
+                        !this.state.validUsername &&
+                        <div className="form-group">
+                            <label htmlFor="usernameInput">Username</label>
+                            <input
+                                id="usernameInput"
+                                className="form-control is-invalid"
+                                placeholder="Enter Username"
+                                onChange={(e) => this.setState({ username: e.target.value })} />
+                            <p className="invalid-feedback">Invalid Username</p>
+                        </div>
+                    }
+                    {
+                        this.state.validPassword &&
+                        <div className="form-group">
+                            <label htmlFor="passwordInput">Password</label>
+                            <input
+                                type="password"
+                                required
+                                id="passwordInput"
+                                className="form-control"
+                                placeholder="Enter Password"
+                                onChange={(e) => this.setState({ password: e.target.value })} />
+                        </div>
+                    }
+                    {
+                        !this.state.validPassword &&
+                        <div className="form-group">
+                            <label htmlFor="passwordInput">Password</label>
+                            <input
+                                type="password"
+                                required
+                                id="passwordInput"
+                                className="form-control is-invalid"
+                                placeholder="Enter Password"
+                                onChange={(e) => this.setState({ password: e.target.value })} />
+                            <p className="invalid-feedback">Invalid Password</p>
+                        </div>
+                    }
                 </form>
 
+                <div className="float-left">
                 <button
-                    onClick={this.login}
+                    onClick={this.validate}
                     className="btn btn-primary">
                     Login
                 </button>
-                <Link to="/registration">Sign up</Link>
+                </div>
+                <div className="float-right">
+                    <Link to="/registration">Sign up</Link>
+                </div>
             </div>
         )
     }
--- src/components/RegisterComponent.js
+++ src/components/RegisterComponent.js
@@ -2,14 +2,30 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default class Register extends React.Component {
-    state = {
-        username: '',
-        password: '',
-        role: 'LEADER',
-        firstName: '',
-        lastName: '',
-        email: ''
+    constructor() {
+        super();
+        this.state = {
+            username: '',
+            password: '',
+            role: '',
+            firstName: '',
+            lastName: '',
+            email: '',
+
+            validation: {
+                validUsername: true,
+                validPassword: true,
+                validRole: true,
+                validFirstName: true,
+                validLastName: true,
+                validEmail: true
+            },
+
+            shouldUpdate: true
+        }
     }
+
+
     register = () => {
         fetch("http://localhost:8080/api/register", {
             // fetch("https://wbdv-team18-final-project.herokuapp.com/api/register", {
@@ -27,87 +43,192 @@ export default class Register extends React.Component {
             .then(currentUser => this.props.history.push("/profile/watchlist"))
     }
 
+    validate = () => {
+        this.setState({
+            shouldUpdate: false,
+            validation: {
+                    validUsername: this.state.username !== "",
+                    validPassword: this.state.password !== "",
+                    validRole: this.state.role !== "",
+                    validFirstName: this.state.firstName !== "",
+                    validLastName: this.state.lastName !== "",
+                    validEmail: this.state.email !== "",
+                }
+            }, () => {
+            if (this.state.validation.validUsername && this.state.validation.validPassword &&
+                this.state.validation.validRole && this.state.validation.validFirstName &&
+                this.state.validation.validLastName && this.state.validation.validEmail) {
+                this.register()
+            }
+        })
+    }
+
     render() {
         return (
             <div>
-                {/* add css padding */}
                 <h1>Register</h1>
-
                 <form>
-                    <div className="form-group ">
-                        <label htmlFor="usernameInput">Username</label>
-                        <input
-                            required
-                            id="usernameInput"
-                            placeholder="Username"
-                            onChange={(e) => this.setState({ username: e.target.value })}
-                            className="form-control" />
-                    </div>
+                    {
+                        this.state.validation.validUsername &&
+                        <div className="form-group ">
+                            <label htmlFor="usernameInput">Username</label>
+                            <input
+                                id="usernameInput"
+                                placeholder="Username"
+                                onChange={(e) => this.setState({ username: e.target.value })}
+                                className="form-control" />
+                        </div>
+                    }
+                    {
+                        !this.state.validation.validUsername &&
+                        <div className="form-group ">
+                            <label htmlFor="usernameInput">Username</label>
+                            <input
+                                id="usernameInput"
+                                placeholder="Username"
+                                onChange={(e) => this.setState({ username: e.target.value })}
+                                className="form-control is-invalid" />
+                            <p className="invalid-feedback">Please choose a username</p>
+                        </div>
+                    }
+                    {
+                        this.state.validation.validPassword &&
+                        <div className="form-group">
+                            <label htmlFor="passwordInput">Password</label>
+                            <input
+                                id="passwordInput"
+                                type="password"
+                                placeholder="Password"
+                                onChange={(e) => this.setState({password: e.target.value})}
+                                className="form-control"/>
+                        </div>
+                    }
+                    {
+                        !this.state.validation.validPassword &&
+                        <div className="form-group">
+                            <label htmlFor="passwordInput">Password</label>
+                            <input
+                                id="passwordInput"
+                                type="password"
+                                placeholder="Password"
+                                onChange={(e) => this.setState({password: e.target.value})}
+                                className="form-control is-invalid"/>
+                            <p className="invalid-feedback">Please choose a password</p>
+                        </div>
+                    }
+                    {
+                        this.state.validation.validRole &&
+                        <div className="form-group ">
+                            <label htmlFor="roleSelect">Role</label>
+                            <select
+                                id="roleSelect"
+                                onChange={(e) => this.setState({ role: e.target.value })}
+                                defaultValue={`CHOOSE`}
+                                className="custom-select custom-select">
+                                <option disabled value="CHOOSE"> Choose User Role</option>
+                                <option value="LEADER"> Group Leader </option>
+                                <option value="MEMBER"> Member </option>
+                            </select>
+                        </div>
+                    }
+                    {
+                        !this.state.validation.validRole &&
+                        <div className="form-group ">
+                            <label htmlFor="roleSelect">Role</label>
+                            <select
+                                id="roleSelect"
+                                onChange={(e) => this.setState({ role: e.target.value })}
+                                defaultValue={`CHOOSE`}
+                                className="custom-select custom-select is-invalid">
+                                <option disabled value="CHOOSE"> Choose User Role</option>
+                                <option value="LEADER"> Group Leader </option>
+                                <option value="MEMBER"> Member </option>
+                            </select>
+                            <p className="invalid-feedback">Please choose a role</p>
+                        </div>
+                    }
+                    {
+                        this.state.validation.validFirstName &&
+                        <div className="form-group ">
+                            <label htmlFor="firstNameInput">First Name</label>
+                            <input
+                                id="firstNameInput"
+                                placeholder="First Name"
+                                onChange={(e) => this.setState({ firstName: e.target.value })}
+                                className="form-control" />
+                        </div>
+                    }
+                    {
+                        !this.state.validation.validFirstName &&
+                        <div className="form-group ">
+                            <label htmlFor="firstNameInput">First Name</label>
+                            <input
+                                id="firstNameInput"
+                                placeholder="First Name"
+                                onChange={(e) => this.setState({ firstName: e.target.value })}
+                                className="form-control is-invalid" />
+                            <p className="invalid-feedback">Please provide your first name</p>
+                        </div>
+                    }
+                    {
+                        this.state.validation.validLastName &&
+                        <div className="form-group ">
+                            <label htmlFor="lastNameInput">Last Name</label>
+                            <input
+                                id="lastNameInput"
+                                placeholder="Last Name"
+                                onChange={(e) => this.setState({ lastName: e.target.value })}
+                                className="form-control" />
+                        </div>
+                    }
+                    {
+                        !this.state.validation.validLastName &&
+                        <div className="form-group ">
+                            <label htmlFor="lastNameInput">Last Name</label>
+                            <input
+                                id="lastNameInput"
+                                placeholder="Last Name"
+                                onChange={(e) => this.setState({ lastName: e.target.value })}
+                                className="form-control is-invalid" />
+                            <p className="invalid-feedback">Please provide your last name</p>
+                        </div>
+                    }
+                    {
+                        this.state.validation.validEmail &&
+                        <div className="form-group ">
+                            <label htmlFor="emailInput">Email</label>
+                            <input
+                                id="emailInput"
+                                placeholder="Email"
+                                onChange={(e) => this.setState({ email: e.target.value })}
+                                className="form-control" />
+                        </div>
+                    }
+                    {
+                        !this.state.validation.validEmail &&
+                        <div className="form-group ">
+                            <label htmlFor="emailInput">Email</label>
+                            <input
+                                id="emailInput"
+                                placeholder="Email"
+                                onChange={(e) => this.setState({ email: e.target.value })}
+                                className="form-control is-invalid" />
+                            <p className="invalid-feedback">Please provide your email</p>
+                        </div>
+                    }
+                </form>
 
-                    <div className="form-group ">
-                        <label htmlFor="passwordInput">Password</label>
-                        <input
-                            required
-                            id="passwordInput"
-                            type="password"
-                            placeholder="Password"
-                            onChange={(e) => this.setState({ password: e.target.value })}
-                            className="form-control" />
-                    </div>
-
-                    <div className="form-group ">
-                        <label htmlFor="roleSelect">Role</label>
-                        <select
-                            required
-                            id="roleSelect"
-                            onChange={(e) => this.setState({ role: e.target.value })}
-                            defaultValue={`CHOOSE`}
-                            className="custom-select custom-select">
-                            <option disabled value="CHOOSE"> Choose User Role</option>
-                            <option value="LEADER"> Group Leader </option>
-                            <option value="MEMBER"> Member </option>
-                        </select>
-                    </div>
-
-                    <div className="form-group ">
-                        <label htmlFor="firstNameInput">First Name</label>
-                        <input
-                            required
-                            id="firstNameInput"
-                            placeholder="First Name"
-                            onChange={(e) => this.setState({ firstName: e.target.value })}
-                            className="form-control" />
-                    </div>
-
-                    <div className="form-group ">
-                        <label htmlFor="lastNameInput">Last Name</label>
-                        <input
-                            required
-                            id="lastNameInput"
-                            placeholder="Last Name"
-                            onChange={(e) => this.setState({ lastName: e.target.value })}
-                            className="form-control" />
-                    </div>
-
-                    <div className="form-group ">
-                        <label htmlFor="emailInput">Email</label>
-                        <input
-                            required
-                            id="emailInput"
-                            placeholder="Email"
-                            onChange={(e) => this.setState({ email: e.target.value })}
-                            className="form-control" />
-                    </div>
-
+                <div className="float-left">
                     <button
                         type="submit"
-                        onClick={this.register}
+                        onClick={this.validate}
                         className="btn btn-primary">
                         Register
                     </button>
-                </form>
-
-                <Link to="/login">Sign in</Link>
+                </div>
+                <div className="float-right">
+                    <Link to="/login">Sign in</Link>
+                </div>
             </div>
         )
     }
